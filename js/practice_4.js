2edeaf970c6f4f6487bbfb8f8ae968d3dab18f30
haroldcalvo <147241098+haroldcalvo@users.noreply.github.com>
2024-03-02 01:23:16 +0800

    update

--- js/practice_4.js
+++ js/practice_4.js
@@ -1,8 +1,9 @@
+$(document).ready(function () {
 let firstModal = $('#firstModal');
 let buttonOpen = $('#ButtonOpen');
 let closeDialog = $('#closeDialog');
 
-$(document).ready(function () {
+
   buttonOpen.click(function () {
     if (firstModal.prop('hidden')) {
       firstModal.prop('hidden', false);
@@ -15,7 +16,17 @@ $(document).ready(function () {
       firstModal[0].close();
     }
   });
-});
+
+var Methods ={
+  checkValue : function(nameValue){
+    let inputNames = $('#Name');
+    if(nameValue === ''){
+      inputNames.addClass('InputStatus');
+    }else{
+      inputNames.removeCLass('InputStatus');
+    }
+  }
+}
 
 var Person = function(age , name, height){
   this.age = age; 
@@ -23,32 +34,20 @@ var Person = function(age , name, height){
   this.height = height; 
 
 };
-// let firstPerson = new Person (21 ,'Harold', '167cm');
-// console.log(`Hi I'm ${firstPerson.name} and I'm ${firstPerson.age} rigth now and my height is ${firstPerson.height}`);
-
-// let userInputName = prompt("Please input your name: ");
-// let userInputAge = prompt("Please input your age: ");
-// let userInputHeight = prompt("Please input your height: ");
-// let InputPerson = new Person(userInputAge, userInputName, userInputHeight);
-// console.log(`Hi I'm ${InputPerson.name}, I'm ${InputPerson.age} year's old, and hy height is ${InputPerson.height} cm`);
-
-let appendInput = (function(){
-
-console.log();
-
-})
-appendInput()
-
 let submitButton = $('#submit');
-
 submitButton.click(function(){
-var InputPerson = new Person(inputAge, inputName, inputHeight);
-var inputName = $('#Name');
-var inputAge = $('#Age');
-var inputHeight = $('#Height');
+  var inputName = $('#Name').val();
+  var inputAge = $('#Age').val();
+  var inputHeight = $('#Height').val();
 
-  let nameVal = inputName.val();
-  let ageVal = inputAge.val();
-  let heightVal = inputHeight.val();
-  $('#boang').text(`Hi I'm ${nameVal || 'No Value'} and I'm ${ageVal || 'No Value'} rigth now and my height is ${heightVal || 'No Value'}`)
-})
+  
+  Methods.checkValue(inputName);
+  
+    var nameVal = inputName;
+    var ageVal = inputAge || 'null';
+    var heightVal = inputHeight || 'null';
+    $('#boang').text(`Hi I'm ${nameVal} and I'm ${ageVal} rigth now and my height is ${heightVal}`)
+  })
+
+
+});
