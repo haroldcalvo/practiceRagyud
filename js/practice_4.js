ae919642e7ae5c5b1ec7faa9c3379c47a29f2408
haroldcalvo <147241098+haroldcalvo@users.noreply.github.com>
2024-03-03 16:46:51 +0800

    update

--- js/practice_4.js
+++ js/practice_4.js
@@ -3,7 +3,6 @@ let firstModal = $('#firstModal');
 let buttonOpen = $('#ButtonOpen');
 let closeDialog = $('#closeDialog');
 
-
   buttonOpen.click(function () {
     if (firstModal.prop('hidden')) {
       firstModal.prop('hidden', false);
@@ -18,31 +17,37 @@ let closeDialog = $('#closeDialog');
   });
 
 var Methods ={
-  checkValue : function(nameValue){
+  checkValue : function(){
     let inputNames = $('#Name');
-    if(nameValue === ''){
+    let inputNameValue = $('#Name').val();
+    if(inputNameValue === ''){
       inputNames.addClass('InputStatus');
     }else{
-      inputNames.removeCLass('InputStatus');
+      inputNames.removeClass('InputStatus');
     }
+  },
+  addSomeTable : function(){
+    let Tabledata = '';
   }
 }
-
 var Person = function(age , name, height){
   this.age = age; 
   this.name = name; 
   this.height = height; 
 
 };
+
+var inputNames = $('#Name');
+var inputAges = $('#Age');
+var inputHeights = $('#Height')
 let submitButton = $('#submit');
+
 submitButton.click(function(){
+  Methods.checkValue();
   var inputName = $('#Name').val();
   var inputAge = $('#Age').val();
   var inputHeight = $('#Height').val();
 
-  
-  Methods.checkValue(inputName);
-  
     var nameVal = inputName;
     var ageVal = inputAge || 'null';
     var heightVal = inputHeight || 'null';
